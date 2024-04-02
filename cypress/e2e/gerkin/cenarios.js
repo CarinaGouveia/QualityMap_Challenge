dd4e0eda5a34cefd1bd93b63fcb296563a7ce062
Carina Gouveia <carina.barros@hubconexa.com>
2024-04-02 19:33:11 -0300

    correção dos cenários

--- cypress/e2e/gerkin/cenarios.js
+++ cypress/e2e/gerkin/cenarios.js
@@ -1,53 +1,52 @@
 const testDescription = {
-    CriarRegistro:  `
+  CriarRegistro: `
     <div>
       <span style="color: #800080; font-weight: bold;">Funcionalidade: </span>
-      <span style="color: #b22222;"> Criar registro </span>
+      <span style="color: #b22222;">Criar registro</span>
     </div>
     <div>
-      <span style="color: #800080; font-weight: bold;">Cenario 1: </span>
-      <span style="color: #b22222;"> Criar novo usuário com sucesso</span>
+      <span style="color: #800080; font-weight: bold;">Cenário 1: </span>
+      <span style="color: #b22222;">Criar novo usuário com sucesso</span>
     </div>
     <div style="margin-left: 40px;">
       <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
-      <strong>E</strong> preenche todos os dados obrigatórios de registro corretamente <br/>
+      <strong>E</strong> preencha todos os dados obrigatórios de registro corretamente <br/>
       <strong>QUANDO</strong> o usuário clicar em "Registrar" <br/>
-      <strong>ENTAO</strong> uma mensagem sinalizando o sucesso da ação deve ser visualizada"
-      <strong>E</strong> o novo registro é criado com sucesso <br/>
+      <strong>ENTÃO</strong> uma mensagem sinalizando o sucesso da ação deve ser visualizada <br/>
+      <strong>E</strong> o novo registro é criado com sucesso.
     </div>
-    `,
-    VerificacaoDosCamposData:`
+  `,
+  VerificacaoDosCamposData: `
     <div>
-    <span style="color: #800080; font-weight: bold;">Funcionalidade: </span>
-    <span style="color: #b22222;"> Preenchimento do formulário </span>
-  </div>
-  <div>
-    <span style="color: #800080; font-weight: bold;">Cenario 2: </span>
-    <span style="color: #b22222;"> Dados do campo data inserido corretamente </span>
-  </div>
-  <div style="margin-left: 40px;">
-    <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
-    <strong>QUANDO</strong> o usuário selecionar os campos da data de nascimento <br/>
-    <strong>ENTAO</strong> o valor selecionado deve aparecer corretamente de no campo correspondente."
-  </div>
-    ` 
-    ,PreenchimentoIncorretoDadosObrigatorios:`
+      <span style="color: #800080; font-weight: bold;">Funcionalidade: </span>
+      <span style="color: #b22222;">Preenchimento do formulário</span>
+    </div>
     <div>
-    <span style="color: #800080; font-weight: bold;">Funcionalidade: </span>
-    <span style="color: #b22222;"> Preenchimento do formulário </span>
-  </div>
-  <div>
-    <span style="color: #800080; font-weight: bold;">Cenario 3: </span>
-    <span style="color: #b22222;"> Campos obrigatórios não preenchidos corretamente </span>
-  </div>
-  <div style="margin-left: 40px;">
-    <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
-    <strong>QUANDO</strong> o usuário não preencher os campos obrigatórios corretamente <br/>
-    <strong>E</strong> clicar em registar <br/>
-    <strong>ENTAO</strong> uma mensagem de erro deve ser exibida abaixo do campo."
-  </div>
-    `
-  };
-  
-  export default testDescription;
-  
+      <span style="color: #800080; font-weight: bold;">Cenário 2: </span>
+      <span style="color: #b22222;">Dados do campo data inseridos corretamente</span>
+    </div>
+    <div style="margin-left: 40px;">
+      <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
+      <strong>QUANDO</strong> o usuário selecionar os campos da data de nascimento <br/>
+      <strong>ENTÃO</strong> o valor selecionado deve aparecer corretamente no campo correspondente.
+    </div>
+  `,
+  PreenchimentoIncorretoDadosObrigatorios: `
+    <div>
+      <span style="color: #800080; font-weight: bold;">Funcionalidade: </span>
+      <span style="color: #b22222;">Preenchimento do formulário</span>
+    </div>
+    <div>
+      <span style="color: #800080; font-weight: bold;">Cenário 3: </span>
+      <span style="color: #b22222;">Campos obrigatórios não preenchidos corretamente</span>
+    </div>
+    <div style="margin-left: 40px;">
+      <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
+      <strong>QUANDO</strong> o usuário não preencher os campos obrigatórios corretamente <br/>
+      <strong>E</strong> clicar em registrar <br/>
+      <strong>ENTÃO</strong> uma mensagem de erro deve ser exibida abaixo do campo.
+    </div>
+  `
+};
+
+export default testDescription;
