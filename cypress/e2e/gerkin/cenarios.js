fc23db071fc0af1f47abc0d9e65335d243a601b8
Carina Gouveia <carina.barros@hubconexa.com>
2024-04-03 09:12:31 -0300

    Inserido imagens do Allure report

--- cypress/e2e/gerkin/cenarios.js
+++ cypress/e2e/gerkin/cenarios.js
@@ -9,7 +9,7 @@ const testDescription = {
       <span style="color: #b22222;">Criar novo usuário com sucesso</span>
     </div>
     <div style="margin-left: 40px;">
-      <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
+      <strong>DADO</strong> que o usuário acesse a página de registro <br/>
       <strong>E</strong> preencha todos os dados obrigatórios de registro corretamente <br/>
       <strong>QUANDO</strong> o usuário clicar em "Registrar" <br/>
       <strong>ENTÃO</strong> uma mensagem sinalizando o sucesso da ação deve ser visualizada <br/>
@@ -26,7 +26,7 @@ const testDescription = {
       <span style="color: #b22222;">Dados do campo data inseridos corretamente</span>
     </div>
     <div style="margin-left: 40px;">
-      <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
+      <strong>DADO</strong> que o usuário acesse a página de registro <br/>
       <strong>QUANDO</strong> o usuário selecionar os campos da data de nascimento <br/>
       <strong>ENTÃO</strong> o valor selecionado deve aparecer corretamente no campo correspondente.
     </div>
@@ -41,7 +41,7 @@ const testDescription = {
       <span style="color: #b22222;">Campos obrigatórios não preenchidos corretamente</span>
     </div>
     <div style="margin-left: 40px;">
-      <strong>DADO</strong> Dado que o usuário acesse a página de registro <br/>
+      <strong>DADO</strong> que o usuário acesse a página de registro <br/>
       <strong>QUANDO</strong> o usuário não preencher os campos obrigatórios corretamente <br/>
       <strong>E</strong> clicar em registrar <br/>
       <strong>ENTÃO</strong> uma mensagem de erro deve ser exibida abaixo do campo.
